64f0a539510d496226c90aecbd406feb83d0c4cf
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-02-20 23:56:36 +0700

    Обработчик записи

--- src/components/Flight.jsx
+++ src/components/Flight.jsx
@@ -1,30 +1,50 @@
 import image from '../img/67c9e921661452cf8ad11cde7a8124c0.jpeg';
+import {data as json} from '../data';
+import {useEffect, useState} from "react";
 
 export default function Flight() {
+    useEffect(() => {
+        document.title = 'Список космических рейсов';
+    }, []);
+
+    const [list, setList] = useState(json.data);
+
+    const handleRecord = (e) => {
+        const index = Number(e.target.id);
+
+        setList((prevList) => {
+            return prevList.map((item, idx) => idx === index ? {
+                ...item,
+                seats_available: item.seats_available - 1
+            } : item);
+        });
+    };
+
     return (
         <main>
             <section className="gagarin-info">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-6 col-sm-12 gagarin-img">
-                            <img src={image} alt="Марс"/>
+                            <img src={image || ''} alt="Марс"/>
                         </div>
                         <div className="col-md-6 col sm-12">
                             <h1>Список космических рейсов</h1>
-                            <div className="booking-flight">
-                                <div className="number">212521</div>
-                                <div className="arrival">Луна 1</div>
-                                <div className="date">22.03.2025</div>
-                                <div className="amount">10</div>
-                                <a href="#" className="btn btn-primary">Записаться</a>
-                            </div>
-                            <div className="booking-flight">
-                                <div className="number">212522</div>
-                                <div className="arrival">Луна 2</div>
-                                <div className="date">24.03.2025</div>
-                                <div className="amount">6</div>
-                                <a href="#" className="btn btn-primary">Записаться</a>
-                            </div>
+                            {!list ? (
+                                <p>Загрузка...</p>
+                            ) : (
+                                list.map((item, index) => (
+                                    <div className="booking-flight" key={index}>
+                                        <div className="number">{item.flight_number}</div>
+                                        <div className="arrival">{item.destination}</div>
+                                        <div className="date">{item.launch_date}</div>
+                                        <div className="amount">{item.seats_available}</div>
+                                        <button type="button" className="btn btn-primary" id={index}
+                                                onClick={handleRecord} disabled={item.seats_available <= 0}>Записаться
+                                        </button>
+                                    </div>
+                                ))
+                            )}
                             <a href="#" className="btn btn-primary">Добавить рейс</a>
                             <a href="#" className="btn btn-secondary">На главную</a>
                         </div>
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const data = {
+    "data": [
+        {
+            "flight_number": "СФ-103",
+            "destination": "Марс",
+            "launch_date": "2025-05-15",
+            "seats_available": 2
+        },
+        {
+            "flight_number": "СФ-105",
+            "destination": "Юпитер",
+            "launch_date": "2024-06-01",
+            "seats_available": 3
+        }
+    ]
+};
